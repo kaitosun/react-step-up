1ce329a47a0c3a36156270ecf32c833078ea9a97
kaitosun <110577892+kaitosun@users.noreply.github.com>
2022-08-18 12:56:45 +0900

    再レンダリングの仕組みを知り、レンダリングを最適化するまで

--- src/index.js
+++ src/index.js
@@ -3,11 +3,9 @@ import ReactDom from "react-dom";
 
 import App from "./App";
 
-const rootElement = document.getElementById("root");
-const root = createRoot(rootElement);
-
-root.render(
+ReactDom.render(
   <StrictMode>
     <App />
-  </StrictMode>
+  </StrictMode>,
+  document.getElementById("root")
 );
